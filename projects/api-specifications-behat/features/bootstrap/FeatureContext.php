c42cff88494914fdae2849dd867825445978bce3
ktom <1516685+ktom@users.noreply.github.com>
2021-05-15 16:21:28 +0200

    behat non fonctionnal initial setup #4

---
+++ projects/api-specifications-behat/features/bootstrap/FeatureContext.php
@@ -0,0 +1,32 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @Given there are artists with the following details:
+     */
+    public function thereAreArtistsWithTheFollowingDetails(TableNode $artists)
+    {
+        foreach ($artists as $artist) {
+            var_dump($artist);
+        }
+    }
+}
