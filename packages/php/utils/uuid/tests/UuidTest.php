4135a4655e0798db15e8a6564714f2330bef236f
ktom <1516685+ktom@users.noreply.github.com>
2021-05-21 01:54:53 +0200

    improved uuids

--- packages/php/utils/uuid/tests/UuidTest.php
+++ packages/php/utils/uuid/tests/UuidTest.php
@@ -4,70 +4,61 @@ namespace ReggaeFinder\Utils\Uuid\Tests;
 
 use PHPUnit\Framework\TestCase;
 use ReggaeFinder\Utils\Uuid\Exceptions\InvalidUuidException;
-use ReggaeFinder\Utils\Uuid\Uuid;
+use ReggaeFinder\Utils\UuidTesting\FakeUuid;
+use ReggaeFinder\Utils\UuidTesting\UuidTestInterface;
 
 class UuidTest extends TestCase implements UuidTestInterface
 {
     public function test_it_can_generate_a_valid_uuid()
     {
-        $uuid = Uuid::generate();
+        $uuid = FakeUuid::generate();
 
-        $this->assertInstanceOf(Uuid::class, $uuid);
-        $this->assertEquals($uuid, Uuid::fromString((string)$uuid));
+        $this->assertInstanceOf(FakeUuid::class, $uuid);
+        $this->assertEquals($uuid, FakeUuid::fromString((string)$uuid));
     }
 
     public function test_it_can_be_created_from_a_string()
     {
-        $uuid = Uuid::fromString('586ae627-0292-4e84-80d8-92deac923204');
+        $uuid = FakeUuid::fromString('586ae627-0292-4e84-80d8-92deac923204');
 
-        $this->assertInstanceOf(Uuid::class, $uuid);
-    }
-
-    public function test_cannot_pass_uuid_other_than_v4()
-    {
-        $this->expectException(InvalidUuidException::class);
-
-        Uuid::fromString(UuidTestInterface::UUID_V1);
-    }
-
-    public function test_cannot_pass_invalid_uuid()
-    {
-        $this->expectException(InvalidUuidException::class);
-
-        Uuid::fromString('abc123');
+        $this->assertInstanceOf(FakeUuid::class, $uuid);
     }
 
     public function test_it_can_render_as_a_string()
     {
-        $uuid = Uuid::fromString('586ae627-0292-4e84-80d8-92deac923204');
+        $uuid = FakeUuid::fromString('586ae627-0292-4e84-80d8-92deac923204');
 
         $this->assertEquals('586ae627-0292-4e84-80d8-92deac923204', (string)$uuid);
     }
 
-    public function test_it_can_validate_a_V4_string()
-    {
-        $result = Uuid::validate('586ae627-0292-4e84-80d8-92deac923204');
-    }
-
     public function test_it_cannot_be_created_from_uuid_other_than_v4()
     {
-        // TODO: Implement test_it_cannot_be_created_from_uuid_other_than_v4() method.
+        $this->expectException(InvalidUuidException::class);
+
+        FakeUuid::fromString(UuidTestInterface::UUID_V1);
     }
 
     public function test_it_cannot_be_created_from_non_uuid_string()
     {
-        // TODO: Implement test_it_cannot_be_created_from_non_uuid_string() method.
+        $this->expectException(InvalidUuidException::class);
+
+        FakeUuid::fromString(UuidTestInterface::NOT_UUID);
     }
 
+    /**
+     * @dataProvider stringForUuidValidation
+     */
     public function test_it_can_validate_a_string(string $stringToBeValidated, bool $expected)
     {
-        // TODO: Implement test_it_can_validate_a_string() method.
+        $this->assertEquals($expected, FakeUuid::validate($stringToBeValidated));
     }
 
     public function stringForUuidValidation(): array
     {
-        // TODO: Implement stringForUuidValidation() method.
+        return [
+            'uuid_v4' => [UuidTestInterface::UUID_V4, true],
+            'uuid_v1' => [UuidTestInterface::UUID_V1, false],
+            'not_uuid' => [UuidTestInterface::NOT_UUID, false],
+        ];
     }
-
-
 }
