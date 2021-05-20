cfb9aad8c40ef413ebe4b3a34784d6380ed043f1
ktom <1516685+ktom@users.noreply.github.com>
2021-05-21 01:53:07 +0200

    added utiliy with helper for testing doctrine

---
+++ packages/php/utils/doctrine-testing-utils/src/PHPUnit/DoctrineOrmTestCase.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace ReggaeFinder\Utils\DoctrineTestingUtils\PHPUnit;
+
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Loader;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Doctrine\DBAL\Types\Type as DoctrineType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Setup;
+use PHPUnit\Framework\TestCase;
+
+abstract class DoctrineOrmTestCase extends TestCase
+{
+    private static ?EntityManagerInterface $entityManager = null;
+    private ?ORMExecutor $executor = null;
+
+//    public static function setUpBeforeClass(): void
+//    {
+//        foreach (static::doctrineTypes() as $name => $class) {
+//            DoctrineType::addType($name, $class);
+//        }
+//
+//        $config = Setup::createXMLMetadataConfiguration([], isDevMode: true);
+//    }
+//
+//    protected static function doctrineTypes(): array
+//    {
+//        return [];
+//    }
+
+
+    public static function setEntityManager(EntityManagerInterface $entityManager): void
+    {
+        self::$entityManager = $entityManager;
+    }
+
+    public function getEntityManager(): EntityManagerInterface
+    {
+        if (self::$entityManager === null) {
+            throw new \RuntimeException('No entity manager has been set');
+        }
+
+        return self::$entityManager;
+    }
+
+    public function loadFixtures(array $fixtures): void
+    {
+        $fixturesLoader = new Loader();
+        foreach ($fixtures as $fixture) {
+            $fixturesLoader->addFixture($fixture);
+        }
+
+        $this->getExecutor()->execute($fixturesLoader->getFixtures());
+    }
+
+    private function getExecutor(): ORMExecutor
+    {
+        if ($this->executor === null) {
+            $this->executor = new ORMExecutor($this->getEntityManager(), new ORMPurger());
+        }
+
+        return $this->executor;
+    }
+}
